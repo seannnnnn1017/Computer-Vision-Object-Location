fcd6e7ade1fcbbaf118d9e87f67f87dbd9535326
seannnnnn1017 <113087774+seannnnnn1017@users.noreply.github.com>
2023-12-10 22:10:37 +0800

    upload

---
+++ Label_image.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import skimage.segmentation as seg
+import skimage.filters as filters
+import skimage.draw as draw
+import skimage.color as color
+from skimage import io
+import numpy as np
+from skimage.segmentation import mark_boundaries
+from PIL import Image
+import glob
+import os
+from skimage import img_as_ubyte
+
+def implot(image, nrows=1, ncols=1, cmap='gray'):
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(14, 14))
+    ax.imshow(image, cmap='gray')
+    ax.axis('off')
+    return fig, ax
+    
+images = glob.glob('pics' + '/*.jpg')
+
+os.makedirs('labels', exist_ok=True)
+for i in images:
+    image = io.imread(i)
+    image_felzenszwalb = seg.felzenszwalb(image, scale=1000) 
+    segmented_img = mark_boundaries(image, image_felzenszwalb)
+
+    # Convert data type to uint8
+    segmented_img_uint8 = img_as_ubyte(segmented_img)
+
+    # 使用相對路徑保存輸出
+    io.imsave('labels/' + os.path.basename(i)[:-4] + '_segmented.png', segmented_img_uint8)
+
+    # Optional: Show the segmented image
+    implot(segmented_img)
---
+++ search.py
@@ -0,0 +1,68 @@
+import sys
+sys.path.insert(0, "C:/Users/fishd/Desktop/Github/Computer-Vision-Object-Location/chromedriver-win64/chromedriver.exe")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+from selenium.common.exceptions import NoSuchElementException
+import urllib.request
+import os
+
+
+# Create DataFrame with XPaths
+f = pd.DataFrame(index=range(1200), columns=['xpath', 'src'])
+for w in range(1200):
+    f.xpath[w] = '//*[@id="islrg"]/div[1]/div[' + str(w) + ']/a[1]/div[1]/img'
+
+f1 = f.drop(f.index[:1])
+
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+
+
+# Initialize ChromeDriver
+chrome_path = 'C:/Users/fishd/Desktop/Github/Computer-Vision-Object-Location/chromedriver-win64/chromedriver.exe'
+chrome_service = ChromeService(chrome_path)
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+# Get user input for the search term
+word = input("What are you looking for? ")
+url = "http://images.google.com/search?q=" + word + "&tbm=isch&sout=1"
+driver.get(url)
+
+# Scroll down to load more images
+for _ in range(500):
+    driver.execute_script("window.scrollBy(0,10000)")
+
+# Define lambda function to get image src
+def lambd(a):
+    try:
+        image_element = driver.find_element("xpath", a)
+        if image_element.get_attribute("data-src"):
+            return image_element.get_attribute("data-src")
+        elif image_element.get_attribute("src"):
+            return image_element.get_attribute("src")
+        else:
+            return None
+    except NoSuchElementException:
+        return None
+
+# Apply lambda function to get src values
+f1['src'] = f1['xpath'].apply(lambda x: (lambd(x)))
+
+f2 = f1.dropna()
+
+# Create 'pics' directory
+os.makedirs('pics', exist_ok=True)
+
+# Download images
+for i in range(len(f2)):
+    urllib.request.urlretrieve(f2.src.iloc[i], 'pics/image' + str(i) + ".jpg")
+
+# Close the driver
+
+driver.quit()
+print('over')
